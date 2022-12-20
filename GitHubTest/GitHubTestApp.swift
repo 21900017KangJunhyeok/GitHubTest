7d47ee9f62a482b37391ed89b38f79ad2a31a945
JH_Kang <21900017@handong.ac.kr>
2022-12-20 14:18:37 +0900

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by junhyeok KANG on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
